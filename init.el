e4f7f26ea36185c94824f7de7cb66f881a03348b
oyqx17 <44708869@qq.com>
2016-06-17 18:28:08 +0800

    fix path problem

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 
 (defun dotspacemacs/layers ()
   "Configuration Layers declaration.
-You should not put any user code in this function besides modifying the variable
-values."
+  You should not put any user code in this function besides modifying the variable
+  values."
   (setq-default
    ;; Base distribution to use. This is a layer contained in the directory
    ;; `+distribution'. For now available distributions are `spacemacs-base'
@@ -49,38 +49,38 @@ values."
      (syntax-checking :variables syntax-checking-enable-by-default nil)
      (spell-checking :variables spell-checking-enable-by-default nil)
      yaml
-     ;; (ruby :variables ruby-version-manager 'rvm)
+     (ruby :variables ruby-version-manager 'rvm)
      python
      lua
      html
      ;; command-log
      javascript
      (typescript :variables
-                 typescript-fmt-on-save nil
-                 typescript-fmt-tool 'typescript-formatter)
+       typescript-fmt-on-save nil
+       typescript-fmt-tool 'typescript-formatter)
      ;; restclient
      emacs-lisp
      (clojure :variables clojure-enable-fancify-symbols t)
      ranger
      ;; racket
      (spacemacs-layouts :variables layouts-enable-autosave nil
-                        layouts-autosave-delay 300)
+      layouts-autosave-delay 300)
      colors
      (git :variables
-          git-magit-status-fullscreen t
-          magit-push-always-verify nil
-          magit-save-repository-buffers 'dontask
-          magit-revert-buffers 'silent
-          magit-refs-show-commit-count 'all
-          magit-revision-show-gravatars nil)
+      git-magit-status-fullscreen t
+      magit-push-always-verify nil
+      magit-save-repository-buffers 'dontask
+      magit-revert-buffers 'silent
+      magit-refs-show-commit-count 'all
+      magit-revision-show-gravatars nil)
      (ibuffer :variables ibuffer-group-buffers-by 'projects)
      (c-c++ :variables
-            c-c++-default-mode-for-headers 'c++-mode)
+      c-c++-default-mode-for-headers 'c++-mode)
      (auto-completion :variables auto-completion-enable-sort-by-usage t
-                      auto-completion-enable-help-tooltip t
-                      :disabled-for org markdown)
+      auto-completion-enable-help-tooltip t
+      :disabled-for org markdown)
      (chinese :variables chinese-enable-youdao-dict t
-              chinese-enable-fcitx t)
+      chinese-enable-fcitx t)
      zilongshanren
      guanghui
      ruby-on-rails
@@ -92,29 +92,29 @@ values."
    dotspacemacs-additional-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '(counsel-projectile
-                                    magit-gh-pulls
-                                    magit-gitflow
-                                    emmet-mode
-                                    evil-mc
-                                    pangu-spacing
-                                    holy-mode
-                                    skewer-mode
-                                    highlight-indentation
-                                    vi-tilde-fringe
-                                    open-junk-file
-                                    coffee-mode
-                                    evil-tutor
-                                    eyebrowse
-                                    hl-anything
-                                    org-bullets
-                                    smooth-scrolling
-                                    org-repo-todo
-                                    chinese-wbim
-                                    chinese-pyim
-                                    srefactor
-                                    org-download
-                                    org-timer
-                                    livid-mode
+    magit-gh-pulls
+    magit-gitflow
+    emmet-mode
+    evil-mc
+    pangu-spacing
+    holy-mode
+    skewer-mode
+    highlight-indentation
+    vi-tilde-fringe
+    open-junk-file
+    coffee-mode
+    evil-tutor
+    eyebrowse
+    hl-anything
+    org-bullets
+    smooth-scrolling
+    org-repo-todo
+    chinese-wbim
+    chinese-pyim
+    srefactor
+    org-download
+    org-timer
+    livid-mode
                                     ;; org-plus-contrib
                                     org-tree-slide
                                     git-gutter
@@ -171,10 +171,10 @@ values."
 
 (defun dotspacemacs/init ()
   "Initialization function.
-This function is called at the very startup of Spacemacs initialization
-before layers configuration.
-You should not put any user code in there besides modifying the variable
-values."
+  This function is called at the very startup of Spacemacs initialization
+  before layers configuration.
+  You should not put any user code in there besides modifying the variable
+  values."
   ;; This setq-default sexp is an exhaustive list of all the supported
   ;; spacemacs settings.
   (setq-default
@@ -224,10 +224,10 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
-                               :weight normal
-                               :width normal
-                               :powerline-scale 1.1)
+     :size 13
+     :weight normal
+     :width normal
+     :powerline-scale 1.1)
    ;; The leader key
    dotspacemacs-leader-key "SPC"
    ;; The leader key accessible in `emacs state' and `insert state'
@@ -369,14 +369,14 @@ values."
 
 (defun dotspacemacs/user-init ()
   "Initialization function for user code.
-It is called immediately after `dotspacemacs/init'.  You are free to put almost
-any user code here.  The exception is org related code, which should be placed
-in `dotspacemacs/user-config'."
+  It is called immediately after `dotspacemacs/init'.  You are free to put almost
+  any user code here.  The exception is org related code, which should be placed
+  in `dotspacemacs/user-config'."
   (setq configuration-layer--elpa-archives
-        '(("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
-          ("org-cn"   . "https://elpa.zilongshanren.com/org/")
-          ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")))
- 
+    '(("melpa-cn" . "https://elpa.zilongshanren.com/melpa/")
+      ("org-cn"   . "https://elpa.zilongshanren.com/org/")
+      ("gnu-cn"   . "https://elpa.zilongshanren.com/gnu/")))
+  
   ;; https://github.com/syl20bnr/spacemacs/issues/2705
   ;; (setq tramp-mode nil)
   (setq tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
@@ -390,8 +390,8 @@ in `dotspacemacs/user-config'."
 
 (defun dotspacemacs/user-config ()
   "Configuration function.
- This function is called at the very end of Spacemacs initialization after
-layers configuration."
+  This function is called at the very end of Spacemacs initialization after
+  layers configuration."
   ;;解决org表格里面中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
     (when (and (spacemacs/system-is-mac) window-system)
@@ -411,7 +411,7 @@ layers configuration."
     "\\" 'evil-repeat-find-char-reverse
     "[s" (lambda (n) (interactive "p") (dotimes (c n nil) (insert " ")))
     "]s" (lambda (n) (interactive "p")
-           (forward-char) (dotimes (c n nil) (insert " ")) (backward-char (1+ n))))
+     (forward-char) (dotimes (c n nil) (insert " ")) (backward-char (1+ n))))
 
   (bb/define-key company-active-map
     (kbd "C-w") 'evil-delete-backward-word)
@@ -423,7 +423,7 @@ layers configuration."
   ;; http://emacsredux.com/blog/2014/04/05/which-function-mode/
   ;; when editing js file, this feature is very useful
   (setq-default header-line-format
-                '((which-func-mode ("" which-func-format " "))))
+    '((which-func-mode ("" which-func-format " "))))
 
   ;; show smartparens mode will cause Emacs frozen when use swiper...
   ;; (turn-off-show-smartparens-mode)
@@ -434,7 +434,7 @@ layers configuration."
   (defun spacemacs/check-large-file ()
     (when (> (buffer-size) 500000)
       (progn (fundamental-mode)
-             (hl-line-mode -1))))
+       (hl-line-mode -1))))
 
   (add-hook 'find-file-hook 'spacemacs/check-large-file)
   (spacemacs/toggle-automatic-symbol-highlight-on)
@@ -487,25 +487,25 @@ layers configuration."
 
   (define-fringe-bitmap 'right-curly-arrow
     [#b00000000
-     #b00000000
-     #b00000000
-     #b00000000
-     #b01110000
-     #b00010000
-     #b00010000
-     #b00000000])
+    #b00000000
+    #b00000000
+    #b00000000
+    #b01110000
+    #b00010000
+    #b00010000
+    #b00000000])
   (define-fringe-bitmap 'left-curly-arrow
     [#b00000000
-     #b00001000
-     #b00001000
-     #b00001110
-     #b00000000
-     #b00000000
-     #b00000000
-     #b00000000])
+    #b00001000
+    #b00001000
+    #b00001110
+    #b00000000
+    #b00000000
+    #b00000000
+    #b00000000])
   ;; tips:  use diminish-undo to toggle mode l
   (if (configuration-layer/layer-usedp 'helm)
-      (spacemacs/set-leader-keys "rh" 'helm-resume))
+    (spacemacs/set-leader-keys "rh" 'helm-resume))
   (when (configuration-layer/layer-usedp 'helm)
     (spacemacs/set-leader-keys "sj" 'counsel-imenu))
   (when (configuration-layer/layer-usedp 'ivy)
@@ -513,6 +513,8 @@ layers configuration."
   ;; (use-package powerline
   ;;   :config
   ;;   (powerline-center-evil-theme ))
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
   )
 
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
--- layers/guanghui/funcs.el
+++ layers/guanghui/funcs.el
@@ -14,7 +14,7 @@
 
 (defun zilongshanren/octopress-rake (command)
   "run rake commands"
-  (let ((command-str (format "/bin/bash -l -c 'source $HOME/.rvm/scripts/rvm && rvm use ruby 2.0.0  && cd %s && rake %s'" octopress-workdir command)))
+  (let ((command-str (format "/bin/bash -l -c 'source $HOME/.rvm/scripts/rvm && rvm use ruby 2.3.0  && cd %s && rake %s'" octopress-workdir command)))
     (shell-command-to-string command-str)))
 
 (defun zilongshanren/octopress-qrsync (command)
@@ -49,27 +49,27 @@
 (defun zilongshanren/directory-parent (directory)
   (let ((parent (file-name-directory (directory-file-name directory))))
     (if (not (equal directory parent))
-        parent)))
+      parent)))
 
 (defun zilongshanren/jekyll-serve ()
   (interactive)
   (let* ((default-directory
-           (if (string-match "_posts/$" default-directory)
-               (zilongshanren/directory-parent (zilongshanren/directory-parent default-directory))
-             (zilongshanren/directory-parent default-directory)))
-         (buffer (if (get-buffer "*jekyll*")
-                     (switch-to-buffer "*jekyll*")
-                   (ansi-term "/bin/zsh" "jekyll")))
-         (proc (get-buffer-process buffer)))
-    (term-send-string proc "rake generate && rake preview\n")
-    (sit-for 4)
-    (browse-url "http://localhost:4000")))
+   (if (string-match "_posts/$" default-directory)
+     (zilongshanren/directory-parent (zilongshanren/directory-parent default-directory))
+     (zilongshanren/directory-parent default-directory)))
+  (buffer (if (get-buffer "*jekyll*")
+   (switch-to-buffer "*jekyll*")
+   (ansi-term "/bin/zsh" "jekyll")))
+  (proc (get-buffer-process buffer)))
+  (term-send-string proc "rake generate && rake preview\n")
+  (sit-for 4)
+  (browse-url "http://localhost:4000")))
 
 
 ;; Screenshot
 (defun zilongshanren//insert-org-or-md-img-link (prefix imagename)
   (if (equal (file-name-extension (buffer-file-name)) "org")
-      (insert (format "[[%s%s]]" prefix imagename))
+    (insert (format "[[%s%s]]" prefix imagename))
     (insert (format "![%s](%s%s)" imagename prefix imagename))))
 
 (defun zilongshanren/capture-screenshot (basename)
@@ -77,27 +77,27 @@
   same directory as the org-buffer/markdown-buffer and insert a link to this file."
   (interactive "sScreenshot name: ")
   (if (equal basename "")
-      (setq basename (format-time-string "%Y%m%d_%H%M%S")))
+    (setq basename (format-time-string "%Y%m%d_%H%M%S")))
   (setq fullpath
-        (concat (file-name-directory (buffer-file-name))
-                "../source/img/"
-                (file-name-base (buffer-file-name))
-                "_"
-                basename))
+    (concat (file-name-directory (buffer-file-name))
+      "../source/img/"
+      (file-name-base (buffer-file-name))
+      "_"
+      basename))
   (setq relativepath
-        (concat (file-name-base (buffer-file-name))
-                "_"
-                basename
-                ".png"))
+    (concat (file-name-base (buffer-file-name))
+      "_"
+      basename
+      ".png"))
   (if (file-exists-p (file-name-directory fullpath))
-      (progn
-        (setq final-image-full-path (concat fullpath ".png"))
-        (call-process "screencapture" nil nil nil "-s" final-image-full-path)
-        (if (executable-find "convert")
-            (progn
-              (setq resize-command-str (format "convert %s -resize 800x600 %s" final-image-full-path final-image-full-path))
-              (shell-command-to-string resize-command-str)))
-        (zilongshanren//insert-org-or-md-img-link "https://zilongshanren.com/img/" relativepath))
+    (progn
+      (setq final-image-full-path (concat fullpath ".png"))
+      (call-process "screencapture" nil nil nil "-s" final-image-full-path)
+      (if (executable-find "convert")
+        (progn
+          (setq resize-command-str (format "convert %s -resize 800x600 %s" final-image-full-path final-image-full-path))
+          (shell-command-to-string resize-command-str)))
+      (zilongshanren//insert-org-or-md-img-link "https://zilongshanren.com/img/" relativepath))
     (progn
       (call-process "screencapture" nil nil nil "-s" (concat basename ".png"))
       (zilongshanren//insert-org-or-md-img-link "./" (concat basename ".png"))))
@@ -116,9 +116,9 @@
   (save-excursion
     (end-of-line)
     (if (looking-back ";")
-        (progn
-          (backward-char)
-          (delete-char 1)))))
+      (progn
+        (backward-char)
+        (delete-char 1)))))
 
 (defun zilongshanren/insert-comma-at-the-end-of-this-line ()
   (interactive)
@@ -131,18 +131,18 @@
   (save-excursion
     (end-of-line)
     (if (looking-back ",")
-        (progn
-          (backward-char)
-          (delete-char 1)))))
+      (progn
+        (backward-char)
+        (delete-char 1)))))
 
 (defmacro zilongshanren|toggle-company-backends (backend)
   "Push or delete the backend to company-backends"
   (let ((funsymbol (intern (format "zilong/company-toggle-%S" backend))))
     `(defun ,funsymbol ()
-       (interactive)
-       (if (eq (car company-backends) ',backend)
-           (setq-local company-backends (delete ',backend company-backends))
-         (push ',backend company-backends)))))
+     (interactive)
+     (if (eq (car company-backends) ',backend)
+       (setq-local company-backends (delete ',backend company-backends))
+       (push ',backend company-backends)))))
 
 (defun zilongshanren/load-my-layout ()
   (interactive)
@@ -160,29 +160,29 @@
   (let (hist-cmd collection val)
     (shell-command "history -r") ; reload history
     (setq collection
-          (nreverse
-           (split-string (with-temp-buffer (insert-file-contents (file-truename "~/.bash_history"))
-                                           (buffer-string))
-                         "\n"
-                         t)))
+      (nreverse
+       (split-string (with-temp-buffer (insert-file-contents (file-truename "~/.bash_history"))
+         (buffer-string))
+       "\n"
+       t)))
     (when (and collection (> (length collection) 0)
-               (setq val (if (= 1 (length collection)) (car collection)
-                           (ivy-read (format "Bash history:") collection))))
-      (kill-new val)
-      (message "%s => kill-ring" val))))
+     (setq val (if (= 1 (length collection)) (car collection)
+       (ivy-read (format "Bash history:") collection))))
+    (kill-new val)
+    (message "%s => kill-ring" val))))
 
   ;; my fix for tab indent
-(defun zilongshanren/indent-region(numSpaces)
-  (progn
+  (defun zilongshanren/indent-region(numSpaces)
+    (progn
                                       ; default to start and end of current line
-    (setq regionStart (line-beginning-position))
-    (setq regionEnd (line-end-position))
+                                      (setq regionStart (line-beginning-position))
+                                      (setq regionEnd (line-end-position))
 
                                       ; if there's a selection, use that instead of the current line
-    (when (use-region-p)
-      (setq regionStart (region-beginning))
-      (setq regionEnd (region-end))
-      )
+                                      (when (use-region-p)
+                                        (setq regionStart (region-beginning))
+                                        (setq regionEnd (region-end))
+                                        )
 
     (save-excursion                          ; restore the position afterwards
       (goto-char regionStart)                ; go to the start of region
@@ -194,25 +194,25 @@
       (setq deactivate-mark nil)           ; restore the selected region
       )
     )
-  )
+    )
 
 
-(defun zilongshanren/tab-region (N)
-  (interactive "p")
-  (if (use-region-p)
+  (defun zilongshanren/tab-region (N)
+    (interactive "p")
+    (if (use-region-p)
       (zilongshanren/indent-region 4)               ; region was selected, call indent-region
     (insert "    ")                   ; else insert four spaces as expected
     ))
 
-(defun zilongshanren/untab-region (N)
-  (interactive "p")
-  (zilongshanren/indent-region -4))
+  (defun zilongshanren/untab-region (N)
+    (interactive "p")
+    (zilongshanren/indent-region -4))
 
-(defun zilongshanren/hack-tab-key ()
-  (interactive)
-  (local-set-key (kbd "<tab>") 'zilongshanren/tab-region)
-  (local-set-key (kbd "<S-tab>") 'zilongshanren/untab-region)
-  )
+  (defun zilongshanren/hack-tab-key ()
+    (interactive)
+    (local-set-key (kbd "<tab>") 'zilongshanren/tab-region)
+    (local-set-key (kbd "<S-tab>") 'zilongshanren/untab-region)
+    )
 
 ;; I'm don't like this settings too much.
 ;; (add-hook 'prog-mode-hook 'zilongshanren/hack-tab-key)
@@ -220,11 +220,11 @@
   "Like `fill-paragraph', but unfill if used twice."
   (interactive)
   (let ((fill-column
-         (if (eq last-command 'endless/fill-or-unfill)
-             (progn (setq this-command nil)
-                    (point-max))
-           fill-column)))
-    (call-interactively #'fill-paragraph)))
+   (if (eq last-command 'endless/fill-or-unfill)
+     (progn (setq this-command nil)
+      (point-max))
+     fill-column)))
+  (call-interactively #'fill-paragraph)))
 
 ;; http://blog.binchen.org/posts/new-git-timemachine-ui-based-on-ivy-mode.html
 (defun my-git-timemachine-show-selected-revision ()
@@ -232,15 +232,15 @@
   (interactive)
   (let (collection)
     (setq collection
-          (mapcar (lambda (rev)
+      (mapcar (lambda (rev)
                     ;; re-shape list for the ivy-read
                     (cons (concat (substring (nth 0 rev) 0 7) "|" (nth 5 rev) "|" (nth 6 rev)) rev))
-                  (git-timemachine--revisions)))
+      (git-timemachine--revisions)))
     (ivy-read "commits:"
-              collection
-              :action (lambda (rev)
-                        (progn (git-timemachine-show-revision rev)
-                               (evil-emacs-state))))))
+      collection
+      :action (lambda (rev)
+        (progn (git-timemachine-show-revision rev)
+         (evil-emacs-state))))))
 
 (defun my-git-timemachine ()
   "Open git snapshot with the selected version.  Based on ivy-mode."
